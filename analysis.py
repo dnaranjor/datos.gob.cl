20839b7e7ddb8ea09f705dffa9e56a063c2ea831
Dennis J. Naranjo <dnaranjor@gmail.com>
2022-12-14 22:30:47 -0300

    Added extra code and playing w/Pandas

--- analysis.py
+++ analysis.py
@@ -3,4 +3,22 @@ import requests
 import pandas as pd
 
 # Solicitar al usuario una cadena para ser consultada
-val = input
+val = input('What is your request (type an string to be searched)?\n')
+
+# Obtener de la fuente de datos todos los documentos que cumplan con el parametro entregado por el usuario
+
+print()
+print('... Buscando ...')
+print()
+response = requests.get("https://datos.gob.cl/api/3/action/package_search?q=" + val)
+#print(response.json())
+
+# Los datos obtenidos son del tipo requests.models.Response, asi que extraeremos los metadatos
+
+jsondata = response.json()
+
+# y luego, comenzamos a jugar con Pandas y los datos obtenidos
+
+rawdf = pd.DataFrame.from_dict(jsondata)
+
+rawdf.head()
